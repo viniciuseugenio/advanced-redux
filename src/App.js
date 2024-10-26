5527f7c47c43b1e4beecd6881ab40740390d82fa
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-10-26 18:36:31 -0300

    feat: display cart according to slice state and allow user to open it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
 
 function App() {
+  const cartIsOpen = useSelector((state) => state.ui.cartIsOpen);
+
   return (
     <Layout>
-      <Cart />
+      {cartIsOpen && <Cart />}
       <Products />
     </Layout>
   );
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
 import classes from "./CartButton.module.css";
+import { useDispatch } from "react-redux";
+import { uiActions } from "../../store/ui.js";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  function handleOpenCart() {
+    dispatch(uiActions.toggleCart());
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={handleOpenCart}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
