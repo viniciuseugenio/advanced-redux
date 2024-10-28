1cea2166f34fe6310d07a2e6902d2fe30c8d58c7
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-10-28 16:50:08 -0300

    refactor: send cart data through a thunk function to avoid bloated components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { sendCartData } from "./store/cart.js";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import { useSelector } from "react-redux";
-import { DB_URL } from "./config.js";
-import { uiActions } from "./store/ui";
-import { useDispatch } from "react-redux";
 
 let isInitial = true;
 
@@ -17,48 +16,13 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    async function fetchCart() {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Uploading cart data!",
-        }),
-      );
-
-      const response = await fetch(DB_URL, {
-        method: "PUT",
-        body: JSON.stringify(cart),
-      });
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed!");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Cart uploaded successfully",
-        }),
-      );
-    }
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    fetchCart().catch(() => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Sending cart data failed!",
-        }),
-      );
-    });
-  }, [cart]);
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
 
   return (
     <>
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui.js";
+import { DB_URL } from "../config.js";
 
 const initialState = { items: [], totalQuantity: 0, upload: false };
 
@@ -37,6 +39,49 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export function sendCartData(cart) {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Uploading cart data!",
+      }),
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(DB_URL, {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Cart uploaded successfully",
+        }),
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending cart data failed!",
+        }),
+      );
+    }
+  };
+}
 
 export const cartActions = cartSlice.actions;
 
